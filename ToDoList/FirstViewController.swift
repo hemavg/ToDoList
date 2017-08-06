4c9baef3b1fe2fe043cc6d866800447e7fbefc1c
hema shamala <hema@LearniOSFreecoms-Mac.local>
2017-08-06 17:52:39 -0400

    First Commit

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,12 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+ var list = ["Buy milk","Make Breakfast","Run to office"]
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+   
+    
+    var myIndex = 0
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        
+        let cell1 = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell1")
+        cell1.textLabel?.text = list[indexPath.row]
+        
+        return (cell1)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+            
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
--- ToDoList/SecondViewController.swift
+++ ToDoList/SecondViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func addItem(_ sender: AnyObject) {
+    
+        if(input.text != "")
+        {
+            list.append(input.text!)
+            input.text = ""
+        }
+    
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
